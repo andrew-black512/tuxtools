f403d0f1aa7513314ae8fa090cbe457f01161615
Andrew Black <andrew@black1.org.uk>
2024-09-01 08:42:58 +0100

    Reformat call to search (multi line) and  add semi-param

--- get_flickr_mb.py
+++ get_flickr_mb.py
@@ -6,11 +6,20 @@ import flickr_config
 import pprint
 
 extra_params={}
+# For command line
 user_id='lesleyablack' #'andrewblack'
+min_taken_date = '2024-08-29'
+
+
 
 flickr = flickrapi.FlickrAPI(flickr_config.API_KEY, flickr_config.API_SECRET, format='parsed-json')
 date_prev = ''
-photos = flickr.photos.search(user_id=user_id,extras='date_taken,description,geo,tags,machine_tags,url_m',**extra_params)
+photos = flickr.photos.search(user_id=user_id, \
+                            #tags=tags, \
+                            min_taken_date =min_taken_date, \
+                            extras='date_taken,description,geo,\
+                            tags,machine_tags,url_m',\
+                            **extra_params)
 for photo in photos['photos']['photo']:
     date = photo['datetaken'][0:10]
 
