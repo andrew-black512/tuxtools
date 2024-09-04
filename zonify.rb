916de2ac6ccca7078142bf18805dd2fe2da7262b
Andrew Black <andrew@black1.org.uk>
2024-09-04 22:11:04 +0100

    new file:   zonify.rb

---
+++ zonify.rb
@@ -0,0 +1,46 @@
+#!/usr/bin/ruby
+require 'fileutils'
+
+# TODO
+#   - tidy glob (eg .TXT - *.TXT)
+#   - allow different formats
+
+def createdir (dirname)
+    if ! Dir.exist?(dirname)
+        puts "create #{dirname}"
+        FileUtils.mkdir_p(dirname) 
+    end
+
+end
+
+
+#---------------------------------------------------------------------------
+def helpandexit
+    puts <<HELP 
+    
+    zoneify.rb  filemask [format]
+        (remember to quote regep)
+        
+HELP
+    exit
+end
+#---------------------------------------------------------------------------
+formats = {
+    'ymd' =>  '%Y_%m_%d' ,
+    'yw'  =>  '%Y_w%V'   ,
+}
+#---------------------------------------------------------------------------
+
+helpandexit if ARGV.count == 0 
+fileglob = ARGV.shift
+
+format = ARGV.count == 1 ? ARGV.shift  :   '%Y_%m_%d'
+
+Dir.glob(fileglob).each do |filename|
+   f=File.new filename
+   dirname = f.mtime.strftime( format )  # _%d configurable?s
+   createdir dirname
+
+   puts "  Move #{filename} to #{dirname}" 
+   FileUtils.mv( filename, dirname + '/' ) 
+end
