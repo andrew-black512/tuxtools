a531f3462f0ea07212ef6c36f898589cdacff345
Andrew Black <andrew@black1.org.uk>
2024-10-01 14:33:18 +0100

    -diags

--- scone.py
+++ scone.py
@@ -5,9 +5,9 @@ def pronounce_scone() :
     # TODO make confugurable, but on what 
     prob = .25 
 
-    words = [ "skoʊn (as in code)","skɒn (o as in body)", ]
+    words = [ "skoʊn (o as in code)","skɒn (o as in body)", ]
     ra = random()  # 0<=x<1 
-    print(F"{ra}")
+    #print(F"{ra}")
     selector = 0 if  ra > prob else 1
     print(F"Pronounce as {words[selector]}")
     print()
