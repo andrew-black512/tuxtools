e1b6b10b6bed2443de33bbcd7f369f0a0581a2b5
Andrew Black <andrew@black1.org.uk>
2024-10-03 22:52:54 +0100

    move intend to class

--- library/headingprinter.py
+++ library/headingprinter.py
@@ -2,15 +2,20 @@ class HeadingPrinter:
     """Encapsulates the functionality of printing headings."""
 
     def __init__(self, offset ):
+        """Creates a heading object
+
+        Args:
+            offset (int): The offset for indentation.
+        
+        """
         self.previous_title = None
         self.offset = offset
 
-    def print_heading(self, title, offset):
+    def print_heading(self, title):
         """Prints the heading if it's different from the previous one.
 
         Args:
             title (str): The title to be printed.
-            offset (int): The offset for indentation.
         """
 
         if title != self.previous_title:
@@ -21,7 +26,7 @@ class HeadingPrinter:
 heading_printer = HeadingPrinter(10)
 heading_printer_2 = HeadingPrinter(11)
 
-heading_printer.print_heading("aaa",10)
-heading_printer.print_heading("aaa",10)
-heading_printer_2.print_heading("xxx",20)
-heading_printer.print_heading("bb",10)
+heading_printer.print_heading("aaa")
+heading_printer.print_heading("aaa")
+heading_printer_2.print_heading("xxx")
+heading_printer.print_heading("bb")
