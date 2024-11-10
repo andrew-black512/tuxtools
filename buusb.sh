a4adf3d0e312ff795f9d96d118fc5a1afe5ef052
Andrew Black <andrew@black1.org.uk>
2024-11-10 20:14:57 +0000

    add user

--- buusb.sh
+++ buusb.sh
@@ -1,9 +1,10 @@
 
 SUB=$1  # relatvei to ~/
-TOPDIR=$HOSTNAME   
+TOPDIR=$HOSTNAME/$USER   
 DRIVE=$(locate_bu_device.sh)
 
 DEST=$DRIVE/$TOPDIR
+echo $DEST
 mkdir -p $DEST/$SUB
 rsync ~/$SUB/  $DEST/$SUB  -va
 #tree -d $DEST
