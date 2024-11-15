9d30e4a45b41f9630c207eb8460a280e0758560f
Andrew Black <andrew@black1.org.uk>
2024-11-15 02:07:46 +0000

    Animal backup

--- backups/bu_animals.sh
+++ backups/bu_animals.sh
@@ -1,9 +1,23 @@
 
+if [[ $# -lt 1 ]]; then
+    echo "Error: Not enough arguments provided."
+    echo "Usage: $0 <argument1>  ..."
+    echo "Please provide at least 1 argument1."
+    exit 1
+fi
 SUB=$1  # relatvei to ~/
-TOPDIR=$HOSTNAME   
-DRIVE="animals@tallis.local:Public/backups"
+OPTS='--delete-during -a -v'
 
 DEST=$DRIVE/$TOPDIR
-mkdir -p $DEST/$SUB
-rsync ~/$SUB/  $DEST/$SUB  -va
+##mkdir -p $DEST/$SUB
+
+TOPDIR=$HOSTNAME   
+#DRIVE="animals@tallis.local:Public/backups"
+DRIVE="animals@localhost:Public/backups"
+
+DEST=$DRIVE/$TOPDIR
+##mkdir -p $DEST/$SUB
+set -x
+rsync ~/$SUB/  $DEST/$SUB  $OPTS
+
 #tree -d $DEST
--- backups/bu_net_all.sh
+++ backups/bu_net_all.sh
@@ -1,10 +1,6 @@
- 2022  python3 ./tag.py
- 2023  python3 ./tag.py this is 
- 2024  cd ..
- 2025  cd backups/
- 2026  ls
- 2027  ./bu_animals.sh 
- 2028* /
- 2029  ./bu_animals.sh Documents 
- 2030  history 10 >> buall.sh
- 2031  history 10 >> bu_net_all.sh
+./bu_animals.sh Documents 
+./bu_animals.sh work
+./bu_animals.sh dev
+
+
+
