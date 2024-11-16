d44db738a3b48ea08c18fe6ac73fd9a9fc69dcf2
Andrew Black <andrew@black1.org.uk>
2024-11-16 18:00:14 +0000

    Changeto TUXLOC (part)

--- shared_aliases.sh
+++ shared_aliases.sh
@@ -1,4 +1,8 @@
-LOC=~andrew/Public/progs_shared/
+
+set -x
+TUXLOC=$(dirname "$BASH_SOURCE")
+
+
 alias flick='$LOC/tuxtools/get_flickr_mb.py'
 alias scone='$LOC/tuxtools/scone.py'
 alias lspdf='$LOC/tuxtools/lspdf.py'
@@ -8,3 +12,4 @@ alias pdfannot='$LOC/tuxtools/pdfannot.py'
 alias cdtux='cd $LOC/tuxtools' 
 
 PATH=$PATH:$LOC/tuxtools
+set +x
