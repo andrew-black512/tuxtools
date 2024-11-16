d64a97754c641d06cce127335bac678159a4932c
Andrew Black <andrew@black1.org.uk>
2024-11-16 17:03:41 +0000

    new file:   shared_aliases.sh

---
+++ shared_aliases.sh
@@ -0,0 +1,10 @@
+LOC=~andrew/Public/progs_shared/
+alias flick='$LOC/tuxtools/get_flickr_mb.py'
+alias scone='$LOC/tuxtools/scone.py'
+alias lspdf='$LOC/tuxtools/lspdf.py'
+alias lspdf='$LOC/tuxtools/lspdf.py'
+alias pdfannot='$LOC/tuxtools/pdfannot.py'
+
+alias cdtux='cd $LOC/tuxtools' 
+
+PATH=$PATH:$LOC/tuxtools
