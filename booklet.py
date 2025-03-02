0720b118d3c8df46f5a010d9ce4dc4fcfa615bee
Andrew Black <andrew@black1.org.uk>
2025-03-02 13:27:03 +0000

    add diagnosticsO

--- booklet.py
+++ booklet.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 import sys
 
-p=int(sys.argv[1])
+p=int(sys.argv[1]) # Get page no from command line (argv)
+
 for i in range(0,int(p/2),2):
-   print( f"{p-i},{i+1},{i+2},{p-i-1},",end=" " ) 
+   print( f"Spread {i}")
+   print( f"{p-i},{i+1},{i+2},{p-i-1},",  end=" " ) 
+   print()
 
 print('')
